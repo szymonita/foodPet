34af6e2b7d59a352596d398d4bd3c7f05eea7c72
szymon <szymon@MacBook-Air-szymon.local>
2020-08-03 21:45:18 +0200

    [Menu] Adding init menu

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { MatCardModule } from '@angular/material/card';
 import { MatInputModule } from '@angular/material/input';
 import { MatButtonModule } from '@angular/material/button';
 import { ReactiveFormsModule } from '@angular/forms';
+import { MatMenuModule } from '@angular/material/menu';
 
 import { MatDialogModule } from '@angular/material/dialog';
 import { LoginErrorMessageDialog } from './dialogs/login-error-message';
@@ -40,6 +41,7 @@ import { PetComponent } from './pet/pet.component';
     MatInputModule,
     MatButtonModule,
     ReactiveFormsModule,
+    MatMenuModule,
     MatDialogModule
   ],
   providers: [],
