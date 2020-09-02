f92126ce1c5dd6097d410443beed627d689f4de2
szymon <szymon@MacBook-Air-szymon.local>
2020-09-02 19:49:30 +0200

    [Feature] Forgot password form

--- src/app/components/forgot-password/forgot-password.component.ts
+++ src/app/components/forgot-password/forgot-password.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-forgot-password',
@@ -6,10 +7,21 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./forgot-password.component.css']
 })
 export class ForgotPasswordComponent implements OnInit {
+  forgotPasswordForm: FormGroup;
 
-  constructor() { }
+  constructor(private formBuilder: FormBuilder) { }
 
   ngOnInit(): void {
+    this.forgotPasswordForm = this.formBuilder.group({
+      email: ['', [
+        Validators.required,
+        Validators.email
+      ]]
+    });
+  }
+
+  forgotPasswordSendForm() {
+    
   }
 
 }
